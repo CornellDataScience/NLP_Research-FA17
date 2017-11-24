ef9245eef8890fc8234c3764124d039ac7b7beca
Cameron Ibrahim <cai29@cornell.edu>
2017-11-24 17:14:12 -0500

    Updated the make_gutenberg_json.py to produce a more accurate, more expansive corpus using Python NLTK

--- dl_style_transfer/make_gutenberg_json.py
+++ dl_style_transfer/make_gutenberg_json.py
@@ -1,7 +1,7 @@
 from lxml import html
 import requests
-import re
 import pandas as pd
+import nltk
 
 
 def book(title, author, link):
@@ -33,7 +33,28 @@ books = [
          "https://www.gutenberg.org/files/1597/1597-h/1597-h.htm"),
     book("Stories the Iroquois Tell Their Children",
          "Mabel Powers",
-         "https://www.gutenberg.org/files/22096/22096-h/22096-h.htm")
+         "https://www.gutenberg.org/files/22096/22096-h/22096-h.htm"),
+    book("Frankenstein",
+         "Mary Wollstonecraft",
+         "https://www.gutenberg.org/files/84/84-h/84-h.htm"),
+    book("APPLETONS' POPULAR SCIENCE MONTHLY",
+         "WILLIAM JAY YOUMANS",
+         "https://www.gutenberg.org/files/43391/43391-h/43391-h.htm"),
+    book("THE BRIDE OF THE NILE",
+         "Georg Ebers",
+         "https://www.gutenberg.org/files/5529/5529-h/5529-h.htm"),
+    book("Folklore of Scottish Lochs and Springs",
+         "James M. Mackinlay",
+         "https://www.gutenberg.org/files/56034/56034-h/56034-h.htm"),
+    book("THE TRAGEDY OF JULIUS CAESAR",
+         "William Shakespeare",
+         "http://www.gutenberg.org/cache/epub/1785/pg1785.html"),
+    book("THE TRAGEDY OF OTHELLO, MOOR OF VENICE",
+         "William Shakespeare",
+         "http://www.gutenberg.org/cache/epub/1793/pg1793.html"),
+    book("Tragedie of Romeo and Juliet",
+         "William Shakespeare",
+         "http://www.gutenberg.org/cache/epub/2261/pg2261.html")
 ]
 
 frames = []
@@ -44,12 +65,13 @@ for b in books:
     tree = html.fromstring(page.content)
 
     pars = tree.xpath('//p/text()')
-    b['text'] = re.split(r'(?<!\w\.\w.)(?<![A-Z][a-z]\.)(?<=\.|\?)\s',
-                         re.sub("<span.*>", '', "\n".join(pars)))
+    sentences = nltk.sent_tokenize("\n".join(pars).replace("\r", ""))
+    b['text'] = list(map(nltk.word_tokenize, sentences[10:-10]))
     frame = pd.DataFrame(b)
     print(frame.shape)
-    frames.append(frame.iloc[1:, :])
+    frames.append(frame)
 
 frame = pd.concat(frames)
 frame = frame.reset_index()
+print("Total: ", frame.shape)
 frame.to_json("./text_sentences.json", orient="records")
