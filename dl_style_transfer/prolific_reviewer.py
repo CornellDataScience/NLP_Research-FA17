b133e42785a277cf99444482a368fd351326448b
Cameron Ibrahim <cai29@cornell.edu>
2017-11-26 04:47:28 -0500

    Got prolific_reviewer to work after an ongoing fight with filewriting

--- dl_style_transfer/prolific_reviewer.py
+++ dl_style_transfer/prolific_reviewer.py
@@ -2,10 +2,26 @@ import nltk
 import pandas as pd
 import numpy as np
 import json
+import os
+
+frame = pd.read_json("/home/cai29/merged.json", lines=True)
+a = frame['sum(text)'].idxmax()
+print(frame['user_id'][a])
 
 
-print("Lalalalala")
-
-data = json.load(open("/home/cai29/Cornell/CDS/yelp/assets/dataset/review.json"))
-d = pd.DataFrame(data, columns=['user_id', 'text'])
-data.head()
+# largest = -1
+# path = "/home/cai29/user_text_sentences.json"
+# for filename in os.listdir(path):
+#     if filename.endswith(".json"):
+#         print(path + "/" + filename)
+#         file = open(path + '/' + filename)
+#         j = json.load(file)
+#         frame = pd.DataFrame(j, columns=['user_id', 'sum(text)'])
+#         if largest == -1:
+#             largest = frame[frame['sum(text)'].argmax(), :]
+#         else:
+#             a = frame[frame['sum(text)'].argmax(), :]
+#             if a['sum(text)'] > largest['sum(text)']:
+#                 largest = a
+#
+# print(largest)
