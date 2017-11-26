af62a4dc334d85f588e9124699373730857417ae
Cameron Ibrahim <cai29@cornell.edu>
2017-11-26 06:40:05 -0500

    Prolific reviewer generates a text file of the sentences written by the most prolific yelp reviewer

--- dl_style_transfer/prolific_reviewer.py
+++ dl_style_transfer/prolific_reviewer.py
@@ -3,10 +3,22 @@ import pandas as pd
 import numpy as np
 import json
 import os
+import mysql.connector as base
 
-frame = pd.read_json("/home/cai29/merged.json", lines=True)
-a = frame['sum(text)'].idxmax()
-print(frame['user_id'][a])
+# frame = pd.read_json("/home/cai29/merged.json", lines=True)
+# a = frame['sum(text)'].idxmax()
+
+cnct = base.connect(user='cds', password='itsalldatababy', database='yelp_db')
+c = cnct.cursor()
+c.execute("SELECT COUNT(*) FROM review GROUP BY user_id HAVING COUNT(*) > 200")
+print(c.fetchall())
+c.execute("SELECT user_id, GROUP_CONCAT(text SEPARATOR '\n') FROM review GROUP BY user_id HAVING COUNT(*) > 200;")
+data = pd.DataFrame(c.fetchall(), columns=['user_id', 'text'])
+m = data['text'].apply(lambda x: len(nltk.sent_tokenize(x))).idxmax()
+sen = nltk.sent_tokenize(data['text'][m])
+file = open("./yelp_sentences.txt", 'w')
+for l in sen:
+    file.write(l + "\n")
 
 
 # largest = -1
