8673a630bb21782cdba868620343b84de3849c9a
Cameron Ibrahim <cai29@cornell.edu>
2017-11-28 21:10:20 -0500

    Fixed errors in bag o' words generator

--- dl_style_transfer/from_shake_yelp.py
+++ dl_style_transfer/from_shake_yelp.py
@@ -1,8 +1,12 @@
 import numpy as np
 import dl_style_transfer.workspace.data_helpers
+import os
 
-shake = list(open('shake_sentences.txt'))
-yelp = list(open('yelp_sentences.txt'))
+here = os.path.dirname(os.path.abspath(__file__))
+
+yelp = list(open(os.path.join(here, 'yelp_sentences.txt')))
+shake = list(open(os.path.join(here, 'shake_sentences.txt')))
+
 
 col = dict()
 word_to_ind = dict()
@@ -11,13 +15,17 @@ ind_to_word = dict()
 def __line_into_col__(line):
     tokens = dl_style_transfer.workspace.data_helpers.clean_str(line)
     for wor in tokens:
-        if col.get(wor) == None:
+        if col.get(wor) is None:
             col[wor] = 1
         else:
             col[wor] = col[wor] + 1
 
-col = col.items()
-col.sort(key=lambda count: count[1], reverse=True)
+for l1, l2 in zip(yelp, shake):
+    __line_into_col__(l1)
+    __line_into_col__(l2)
+
+lis = list(col.items())
+lis.sort(key=lambda count: count[1], reverse=True)
 for i, word in enumerate(col):
     word_to_ind[word] = i
     ind_to_word[i] = word
@@ -30,7 +38,7 @@ def get_bag():
         for wor in dl_style_transfer.workspace.data_helpers.clean_str(sent):
             bag[j, word_to_ind[wor]] = bag[j, word_to_ind[wor]] + 1
     for j,sent in enumerate(yelp):
-        for wor in sent:
+        for wor in dl_style_transfer.workspace.data_helpers.clean_str(sent):
             bag[j, word_to_ind[wor]] = bag[j, word_to_ind[wor]] + 1
     return bag
 
