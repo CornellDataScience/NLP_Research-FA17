ac9d94b4b78db0a71063b0ef7bf985beb7e5ecf7
Cameron Ibrahim <cai29@cornell.edu>
2017-12-01 19:13:44 -0500

    Fixed errors with ryans_strange_input
    get_bag takes a LOT of memory, use is not advised

--- dl_style_transfer/from_shake_yelp.py
+++ dl_style_transfer/from_shake_yelp.py
@@ -4,48 +4,48 @@ import os
 
 here = os.path.dirname(os.path.abspath(__file__))
 
-yelp = list(open(os.path.join(here, 'yelp_sentences.txt')))
-shake = list(open(os.path.join(here, 'shake_sentences.txt')))
+sents = list(open(os.path.join(here, 'yelp_sentences.txt'))) + list(open(os.path.join(here, 'shake_sentences.txt')))
 
+thresh = 5
 
 col = dict()
 word_to_ind = dict()
 ind_to_word = dict()
 
+
 def __line_into_col__(line):
-    tokens = dl_style_transfer.workspace.data_helpers.clean_str(line)
+    tokens = dl_style_transfer.workspace.data_helpers.clean_str(line).split(" ")
     for wor in tokens:
         if col.get(wor) is None:
             col[wor] = 1
         else:
             col[wor] = col[wor] + 1
 
-for l1, l2 in zip(yelp, shake):
-    __line_into_col__(l1)
-    __line_into_col__(l2)
+
+for l in sents:
+    __line_into_col__(l)
 
 lis = list(col.items())
 lis.sort(key=lambda count: count[1], reverse=True)
-for i, word in enumerate(col):
-    word_to_ind[word] = i
-    ind_to_word[i] = word
+for i, word in enumerate(lis):
+    word_to_ind[word[0]] = i
+    ind_to_word[i] = word[0]
 
 voc_len = len(word_to_ind)
 
+shape = (len(sents), voc_len)
+
 
 def get_bag():
-    bag = np.zeros((len(shake) + len(yelp), voc_len))
-    for j,sent in enumerate(shake):
-        for wor in dl_style_transfer.workspace.data_helpers.clean_str(sent):
+    bag = np.zeros(shape)
+    for j,sent in enumerate(sents):
+        for wor in dl_style_transfer.workspace.data_helpers.clean_str(sent).split(" "):
             bag[j, word_to_ind[wor]] = bag[j, word_to_ind[wor]] + 1
-    for j,sent in enumerate(yelp):
-        for wor in dl_style_transfer.workspace.data_helpers.clean_str(sent):
-            bag[j, word_to_ind[wor]] = bag[j, word_to_ind[wor]] + 1
-    return bag
+    return np.log(1 + bag) / np.max(np.log(1 + bag), axis=0)
 
 
 def string_to_vec(string):
-    tokens = dl_style_transfer.workspace.data_helpers.clean_str(string)
+    tokens = dl_style_transfer.workspace.data_helpers.clean_str(string).split(" ")
     vec = np.zeros(voc_len)
     for wor in tokens:
         vec[word_to_ind[wor]] = vec[word_to_ind[wor]] + 1
@@ -54,11 +54,9 @@ def string_to_vec(string):
 
 def get_ryans_strange_input():
     vec = []
-    for l1, l2 in zip(yelp, shake):
-        f = lambda x: word_to_ind[x]
-        vec.append(map(f, dl_style_transfer.workspace.data_helpers.clean_str(l1)))
-        vec.append(map(f, dl_style_transfer.workspace.data_helpers.clean_str(l2)))
-    return np.array([i for l in vec for i in l])
+    for l in sents:
+        vec.append(dl_style_transfer.workspace.data_helpers.clean_str(l))
+    return np.array([word_to_ind[i] for l in vec for i in l.split(" ")])
 
 
 def vocab_length():
