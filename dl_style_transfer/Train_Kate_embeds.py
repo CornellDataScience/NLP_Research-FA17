06ca3703b921fb39593ce978a70dda0e63f50bcb
Ryan Butler <thebutlah@gmail.com>
2017-12-01 19:17:35 -0500

    Added seed to kate trainer

--- dl_style_transfer/Train_Kate_embeds.py
+++ dl_style_transfer/Train_Kate_embeds.py
@@ -6,10 +6,11 @@ sys.path.append(os.path.abspath('../'))
 import dl_style_transfer.from_shake_yelp as yelp
 from dl_style_transfer.workspace.kate_new import Kate
 
+seed = 1337
+np.random.seed(seed)
 
 x_train = yelp.get_ryans_strange_input()
 kate = Kate(128, yelp.vocab_length(), 32, 6.26)
 kate.train(x_train, 100, 32)
 
-kate.save_model("saved/Saved_Kate.ckpt")
-
+kate.save_model("saved/kate_hot.ckpt")
