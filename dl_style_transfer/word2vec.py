aa9fd8851c3076c868d572c03ceb28c69c225f31
Yuji Akimoto <ya242@cornell.edu>
2017-12-02 07:04:37 -0500

    Initial commit of paper

--- dl_style_transfer/word2vec.py
+++ dl_style_transfer/word2vec.py
@@ -17,4 +17,4 @@ if __name__ == "__main__":
     train_corpus = Corpus(os.getcwd())
     params = {'size': 200, 'window': 10, 'min_count': 10, 'workers': max(1, mp.cpu_count() - 1), 'sample': 1E-3, }
     model = gensim.models.Word2Vec(train_corpus, **params)
-    print(model.wv['liue'])
+    print(model.wv['live'])
