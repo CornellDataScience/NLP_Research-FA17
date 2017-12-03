96100c95d05da4a4f1a7c84565f302634f084e92
Yuji Akimoto <ya242@cornell.edu>
2017-12-03 05:06:17 -0500

    Some shitty code

--- dl_style_transfer/workspace/shallowCNN_reconstruct.py
+++ dl_style_transfer/workspace/shallowCNN_reconstruct.py
@@ -107,8 +107,8 @@ with tf.Graph().as_default():
         data_batch = x_train[:1]
         embeddings_batch = np.zeros(data_batch.shape + [FLAGS.embedding_dim])
         for i, sentence in enumerate(x_train):
-            embeddings_batch[i] = np.array(OneHotEncoder(n_values=len(vocab_processor.vocabulary_)).fit_transform(sentence).todense())
-        
+            embeddings_batch[i] = np.array(OneHotEncoder(n_values=len(vocab_processor.vocabulary_).fit_transform(np.array(sentence).reshape(1, -1))).todense()).reshape(len(vocab_processor.vocabulary_), -1)
+
         # Assign reconstruction tensor to data_batch to generatae target_content
         target_content = sess.run(cnn.activations, feed_dict={cnn.reconstructions: embeddings_batch})
         target_content = [tf.constant(target) for target in target_content]
